d4c3c527b4e43cb5ba00014417027ac11a0f7b5f
fael02 <89419750+fael02@users.noreply.github.com>
2021-08-25 15:27:38 -0300

    Espécies

--- web/database/seeders/EspecieSeeder.php
+++ web/database/seeders/EspecieSeeder.php
@@ -15,8 +15,38 @@ class EspecieSeeder extends Seeder
     public function run()
     {
         $especie                            = new Especie;
-        $especie->nome                      = 'Especie de testes';
-        $especie->descricao                 = 'Características da espécie!!';
+        $especie->nome                      = 'Cachorro';
+        $especie->descricao                 = '';
+        $especie->save();
+
+        $especie                            = new Especie;
+        $especie->nome                      = 'Gato';
+        $especie->descricao                 = '';
+        $especie->save();
+
+        $especie                            = new Especie;
+        $especie->nome                      = 'Furão';
+        $especie->descricao                 = '';
+        $especie->save();
+
+        $especie                            = new Especie;
+        $especie->nome                      = 'Pássaro';
+        $especie->descricao                 = '';
+        $especie->save();
+
+        $especie                            = new Especie;
+        $especie->nome                      = 'Réptil';
+        $especie->descricao                 = '';
+        $especie->save();
+
+        $especie                            = new Especie;
+        $especie->nome                      = 'Roedor';
+        $especie->descricao                 = '';
+        $especie->save();
+
+        $especie                            = new Especie;
+        $especie->nome                      = 'Outra';
+        $especie->descricao                 = '';
         $especie->save();
     }
 }
