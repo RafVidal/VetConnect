04efebb41aa9f1d072fe94a85401d1244b1c5a23
fael02 <89419750+fael02@users.noreply.github.com>
2021-08-24 22:16:45 -0300

    vacinas obrigatorias

--- web/database/seeders/VacinaSeeder.php
+++ web/database/seeders/VacinaSeeder.php
@@ -15,9 +15,52 @@ class VacinaSeeder extends Seeder
     public function run()
     {
         $vacina                             = new Vacina;
-        $vacina->nome                       = 'Vacina teste';
-        $vacina->descricao                  = 'Vacina para testes';
+        $vacina->nome                       = 'V8 ou V10';
+        $vacina->descricao                  = '6 a 8 semanas/ Previni Leptospirose, Cinomose, Hepatite Infecciona Canina, entre outras...';
         $vacina->obrigatoria                = true;
         $vacina->save();
+
+        $vacina                             = new Vacina;
+        $vacina->nome                       = 'V8 ou V10';
+        $vacina->descricao                  = '12 semanas/ Previni Leptospirose, Cinomose, Hepatite Infecciona Canina, entre outras...';
+        $vacina->obrigatoria                = true;
+        $vacina->save();
+
+        $vacina                             = new Vacina;
+        $vacina->nome                       = 'Gripe Canina';
+        $vacina->descricao                  = '12 semanas/ Previni Adenovírus Canino Tipo 2, Parainfluenenza Canina e Bordetella bronchiseptica.';
+        $vacina->obrigatoria                = true;
+        $vacina->save();
+
+        $vacina                             = new Vacina;
+        $vacina->nome                       = 'Giardíase';
+        $vacina->descricao                  = '12 semanas/ Animais em canis,criadores ou locais com muitos cães que vivem em ambiente mais úmido.';
+        $vacina->obrigatoria                = true;
+        $vacina->save();
+
+        $vacina                             = new Vacina;
+        $vacina->nome                       = 'V8 ou V10';
+        $vacina->descricao                  = '16 semanas/ Última dose de reforço';
+        $vacina->obrigatoria                = true;
+        $vacina->save();
+
+        $vacina                             = new Vacina;
+        $vacina->nome                       = 'Gripe Canina';
+        $vacina->descricao                  = '16 semanas/ Dose de reforço da vacina injetável, a intrasanal é aplicada em dose única';
+        $vacina->obrigatoria                = true;
+        $vacina->save();
+
+        $vacina                             = new Vacina;
+        $vacina->nome                       = 'Giardíase';
+        $vacina->descricao                  = '16 semanas/ Segunda dose de reforço.';
+        $vacina->obrigatoria                = true;
+        $vacina->save();
+
+        $vacina                             = new Vacina;
+        $vacina->nome                       = 'Anti-Rábica';
+        $vacina->descricao                  = '16 semanas/ Contra a raiva.';
+        $vacina->obrigatoria                = true;
+        $vacina->save();
+
     }
 }
