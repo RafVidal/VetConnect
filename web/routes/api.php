6edc5d4ac2440f164d08766e0229026fbac7fa2f
fael02 <89419750+fael02@users.noreply.github.com>
2021-10-21 16:58:07 -0300

    nada

--- web/routes/api.php
+++ web/routes/api.php
@@ -29,7 +29,7 @@ Route::group([
 
     Route::post('login', [AuthController::class, 'login'])->name('login');
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+    Route::post('refresh', [AuthController::class, 'refresh'])->name('refresh');;
+    Route::post('me', [AuthController::class, 'me'])->name('me');;
 
 });
