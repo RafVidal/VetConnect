ad5a64869fdc93fdecfd5158ac9f60156a6f74fb
wt2m <52261424+wt2m@users.noreply.github.com>
2022-01-24 15:42:33 -0300

    ç

--- web/app/Http/Controllers/PetsController.php
+++ web/app/Http/Controllers/PetsController.php
@@ -69,8 +69,8 @@ class PetsController extends Controller
     public function show($id)
     {
         $animal = Animal::findorfail ($id);
-        $cartao_de_vacinacao= CartaoDeVacinacao::all();
-        $medicacao= Medicacao::all();
+        $cartao_de_vacinacao= CartaoDeVacinacao::where('animal', $id)->get();
+        $medicacao= Medicacao::where('animal', $id)->get();
         return view('animal.show',['cartao_de_vacinacao'=>$cartao_de_vacinacao,'medicacao'=>$medicacao,'animal'=>$animal]);
     }
 
